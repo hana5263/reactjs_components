e8047468de1a203a441eced17f79bbabab84a60a
Hana <hanakim@bkloud.co.kr>
2019-01-30 16:28:21 +0900

    React Components Task 2

--- src/components/DetailedComponent.js
+++ src/components/DetailedComponent.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
 
@@ -6,12 +8,30 @@ class DishDetail extends Component {
         super(props);
     }
 
+	renderDish(dish) {
+		if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );		
+	}
+	
     render() {
         if (this.props.dish != null) {
             return(
 			<div className="column">
 				<div className="row">
                   <div className="col-12 col-md-5 m-1">
+					{this.renderDish(this.props.dish)}
 				  </div>
 				</div>
 				<div className="row">
