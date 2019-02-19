2e9174ee2f3bb8aa4dd45b09c02a8507fd0a8743
Hana <hanakim@bkloud.co.kr>
2019-02-19 14:50:40 +0900

    Exercises for Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -26,9 +27,11 @@ class Main extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+		<Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
+		<Footer />
+		</div>
     );
   }
 }
