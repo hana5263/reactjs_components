e5b9fa296725423be30325ba5bdf39f36420ae48
Hana <hanakim@bkloud.co.kr>
2019-02-19 17:46:56 +0900

    React Router Task #2

---
+++ src/components/LeaderComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Media } from 'reactstrap';
+
+function RenderLeader({leader}) {
+	if (leader != null)
+		return(
+			<Media>
+				<Media left href='#'>
+					<Media object src={leader.image} alt={leader.name} />
+				</Media>
+				<Media body>
+					<Media heading>
+						{leader.name}
+					</Media>
+					{leader.designation}
+					{leader.description}
+				</Media>
+			</Media>
+		);
+	else
+		return(
+			<div></div>
+		);
+}
+
+export default RenderLeader;
