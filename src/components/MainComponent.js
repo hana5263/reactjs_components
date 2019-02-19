72ac22af371509061d91fcc41d464f619c64ef87
Hana <hanakim@bkloud.co.kr>
2019-02-19 15:48:33 +0900

    Exercise for SPA Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
@@ -32,6 +33,13 @@ class Main extends Component {
 		  />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     
 	return (
 	  <div>
@@ -44,7 +52,8 @@ class Main extends Component {
           <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-            <Route exact path='/contactus' component={Contact} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
+			<Route exact path='/contactus' component={Contact} />} />
 			<Redirect to="/home" />
           </Switch>
         <Footer />
